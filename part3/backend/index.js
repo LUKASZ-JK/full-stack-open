af03620aac82cca72b1d3b6920568101c93714ae
Łukasz Jędrzejczyk <inz.lukasz.jedrzejczyk@gmail.com>
2024-04-04 21:07:35 +0200

    Adds a solution for exercises 3.15-3.18

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -10,9 +10,11 @@ app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 
-const generateId = () => {
-  return Math.floor(Math.random() * 90000) + 5;
-}
+// Not necessary, MongoDB generates Id automatically
+
+// const generateId = () => {
+//   return Math.floor(Math.random() * 90000) + 5;
+// }
 
 morgan.token('body', function (req, res) {
   return req.method === "POST" ? JSON.stringify(req.body) : null
@@ -44,10 +46,12 @@ let persons = [
 ]
 
 app.get('/info', (request, response) => {
-  response.send(`
+  Person.find({}).then(persons => {
+    response.send(`
     <p>Phonebook has info for ${persons.length} people</p>
     <p>${new Date()}</p>
     `)
+  })
 })
 
 app.get('/api/persons', (request, response) => {
@@ -56,10 +60,16 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -86,11 +96,27 @@ app.post('/api/persons', (request, response) => {
   })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
 
-  response.status(204).end()
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -98,6 +124,16 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
