66a06fd8d9d870451760daaf252a5ed4a5e88039
Łukasz Jędrzejczyk <inz.lukasz.jedrzejczyk@gmail.com>
2024-07-23 22:46:42 +0200

    Adds a solution for exercises 6.3-6.8

---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,25 @@
+import { useDispatch } from 'react-redux'
+import { addAnecdote } from '../reducers/anecdoteReducer'
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch()
+  const createAnectode = e => {
+    e.preventDefault()
+    const content = e.target.anecdote.value
+    e.target.anecdote.value = ''
+    dispatch(addAnecdote(content))
+  }
+  return (
+    <>
+      <h2>create new</h2>
+      <form onSubmit={createAnectode}>
+        <div>
+          <input name="anecdote" />
+        </div>
+        <button>create</button>
+      </form>
+    </>
+  )
+}
+
+export default AnecdoteForm
