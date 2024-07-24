a3e4e3ff473b0ac3d1b33c7e2be82e5b86f292aa
Łukasz Jędrzejczyk <inz.lukasz.jedrzejczyk@gmail.com>
2024-07-24 21:04:13 +0200

    Adds a solution for exercise 6.9

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -3,7 +3,9 @@ import { vote } from '../reducers/anecdoteReducer'
 
 const AnecdoteList = () => {
   const dispatch = useDispatch()
-  const anecdotes = useSelector(state => state.sort((a, b) => b.votes - a.votes))
+  const anecdotes = useSelector(({ anecdotes, filter }) =>
+    anecdotes.filter(a => a.content.toLowerCase().includes(filter.toLowerCase())).sort((a, b) => b.votes - a.votes)
+  )
   return (
     <>
       {anecdotes.map(anecdote => (
