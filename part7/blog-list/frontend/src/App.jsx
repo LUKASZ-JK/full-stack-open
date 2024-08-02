593f82a82cceff5ad3cc3decdb7085e53b90a961
Łukasz Jędrzejczyk <inz.lukasz.jedrzejczyk@gmail.com>
2024-08-03 00:19:44 +0200

    Adds a solution for exercise 7.14

--- part7/blog-list/frontend/src/App.jsx
+++ part7/blog-list/frontend/src/App.jsx
@@ -4,10 +4,11 @@ import { useState, useEffect, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { initializeBlogs, createBlog, giveLike, deleteBlog } from './reducers/blogReducer'
 import { initializeUser, loginUser, logoutUser } from './reducers/userReducer'
+import { initializeUsers } from './reducers/usersReducer'
+
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
 import Blog from './components/Blog'
-import blogService from './services/blogs'
-import loginService from './services/login'
 import { Notification } from './components/Notification'
 import Togglable from './components/Togglable'
 import LoginForm from './components/LoginForm'
@@ -29,39 +30,21 @@ const App = () => {
     dispatch(initializeBlogs())
   }, [dispatch])
 
+  useEffect(() => {
+    dispatch(initializeUsers())
+  }, [dispatch])
+
+  const currentUser = useSelector(({ user }) => {
+    return user
+  })
+
   const blogs = useSelector(({ blog }) => {
     return blog
   })
 
-  const user = useSelector(({ user }) => {
-    return user
+  const users = useSelector(({ users }) => {
+    return users
   })
-
-  const loginForm = () => (
-    <LoginForm
-      username={username}
-      password={password}
-      handleUsernameChange={({ target }) => setUsername(target.value)}
-      handlePasswordChange={({ target }) => setPassword(target.value)}
-      handleSubmit={handleLogin}
-    />
-  )
-
-  const blogsSection = () => (
-    <>
-      <h2>blogs</h2>
-      <Togglable buttonLabel="New blog" ref={blogFormRef}>
-        <BlogForm createBlog={addBlog} />
-      </Togglable>
-      <br />
-      <br />
-      <div>
-        {blogs.map(blog => (
-          <Blog key={blog.id} blog={blog} addLike={addLike} currentUser={user} removeBlog={removeBlog} />
-        ))}
-      </div>
-    </>
-  )
 
   const handleLogin = async event => {
     event.preventDefault()
@@ -72,7 +55,7 @@ const App = () => {
 
   const handleLogout = async event => {
     event.preventDefault()
-    dispatch(logoutUser(user))
+    dispatch(logoutUser(currentUser))
   }
 
   const addBlog = async blogObject => {
@@ -90,19 +73,82 @@ const App = () => {
     }
   }
 
-  return (
-    <div>
-      <Notification />
-      {user === null ? (
+  const loginForm = () => (
+    <LoginForm
+      username={username}
+      password={password}
+      handleUsernameChange={({ target }) => setUsername(target.value)}
+      handlePasswordChange={({ target }) => setPassword(target.value)}
+      handleSubmit={handleLogin}
+    />
+  )
+
+  const BlogsSection = () => (
+    <>
+      <h2>blogs</h2>
+      <Togglable buttonLabel="New blog" ref={blogFormRef}>
+        <BlogForm createBlog={addBlog} />
+      </Togglable>
+      <br />
+      <br />
+      <div>
+        {blogs.map(blog => (
+          <Blog key={blog.id} blog={blog} addLike={addLike} currentUser={currentUser} removeBlog={removeBlog} />
+        ))}
+      </div>
+    </>
+  )
+
+  const MainSection = ({ children }) => (
+    <>
+      {currentUser === null ? (
         loginForm()
       ) : (
         <div>
-          <p>{user.name} logged-in</p>
+          <p>{currentUser.name} logged-in</p>
           <button onClick={handleLogout}>Logout</button>
-          {blogsSection()}
+          {children}
         </div>
       )}
-    </div>
+    </>
+  )
+
+  const UsersSection = () => (
+    <>
+      <h2>Users</h2>
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>blogs created</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr key={user.username}>
+              <td>{user.name}</td>
+              <td>{user.blogs.length}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  )
+
+  return (
+    <>
+      <div>
+        <Notification />
+        <MainSection>
+          <Router>
+            <Routes>
+              <Route path="/" element={<BlogsSection />} />
+              <Route path="/users" element={<UsersSection />} />
+            </Routes>
+          </Router>
+        </MainSection>
+      </div>
+    </>
   )
 }
 
